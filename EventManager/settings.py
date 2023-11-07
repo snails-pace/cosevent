a686b32c82c2310a1f03cccddfa5b83284556a25
Lovis <lovis_@systemli.org>
2023-11-07 09:03:08 +0100

    Database migration

--- EventManager/settings.py
+++ EventManager/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'EventManager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "EventManager",
+        "HOST": "localhost",
+        "USER": "root",
+        "PASSWORD": "pCxMU8WFgBux1FVw"
     }
 }
 
