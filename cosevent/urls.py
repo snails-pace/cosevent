923a6be663355e04fcd64b6c9db1413784457851
Lovis <lovis_@systemli.org>
2023-12-03 21:23:03 +0100

    Add my_event_list_view and set permissions

--- cosevent/urls.py
+++ cosevent/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.EventListView.as_view(), name="event_list"),
+    path("", views.event_list_view, name="event_list"),
+    path("my_events", views.my_event_list_view, name="my_events"),
     path("event/<int:pk>", views.EventView.as_view(), name="event"),
     path("event/<int:pk>/update", views.update_event_view, name="event_update"),
     path("event/create", views.create_event_view, name="event_create"),
-    path("event/<int:pk>/delete", views.DeleteEventView.as_view(), name="event_delete"),
+    path("event/<int:pk>/delete", views.event_delete_view, name="event_delete"),
     path("categories", views.category_list_view, name="category_list"),
     path("category/create", views.create_category_view, name="category_create"),
     path("category/<int:pk>/delete", views.category_delete_view, name="category_delete")
--- cosevent/views.py
+++ cosevent/views.py
@@ -1,8 +1,12 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.messages.views import SuccessMessageMixin
+from django.core.exceptions import PermissionDenied
+from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse, reverse_lazy
 from django.views import generic
+from django.views.decorators.http import require_http_methods
 from django.views.generic import TemplateView
 
 from cosevent.forms import UpdateEventForm, UpdateCategoryForm
@@ -14,6 +18,30 @@ class EventListView(generic.ListView):
     model = Event
     context_object_name = 'event_list'
     template_name = 'event_list.html'
+
+
+def event_list_view(request):
+
+    events = Event.objects.all()
+    context = {'event_list': events}
+    return render(request, 'event_list.html', context)
+
+
+@login_required
+@require_http_methods(['GET'])
+def my_event_list_view(request):
+    logged_in_user = request.user
+    try:
+        profile = Profile.objects.get(user=logged_in_user)
+    except Profile.DoesNotExist:
+        profile = None
+    if profile:
+        user_events = Event.objects.filter(owner=profile)
+    else:
+        user_events = Event.objects.all()
+
+    context = {'event_list': user_events}
+    return render(request, 'event_list.html', context)
 
 
 class EventView(generic.DetailView):
@@ -53,50 +81,79 @@ class EventView(generic.DetailView):
 #         return super().form_valid(form)
 
 
-class DeleteEventView(SuccessMessageMixin, generic.DeleteView):
-    model = Event
-    context_object_name = 'event'
-    template_name = 'event_delete.html'
-    success_url = reverse_lazy('event_list')
-    success_message = 'Event deleted'
+
+@login_required
+def event_delete_view(request, pk):
+    event = get_object_or_404(Event, id=pk)
+    profile_id = Profile.objects.get(user=request.user).id
+    # if logged in user is not owner of the event, then raise 403
+    if profile_id != event.owner_id:
+        raise PermissionDenied("You do not have permission to delete this event")
+    if request.method == 'POST':
+        event.delete()
+        messages.success(request, "Event deleted")
+        return redirect('my_events')
+
+    context = {'event': event}
+    return render(request, 'event_delete.html', context)
+
 
 @login_required
 def create_event_view(request):
     if request.method == 'POST':
         event_form = UpdateEventForm(request.POST)
         if event_form.is_valid():
-            event_form.save()
-            return redirect('event_list')
+            try:
+                event_form.save()
+                return redirect('event_list')
+            except Exception as e:
+                print(f"Error occurred in {e}")
+                return HttpResponse(" An error occurred while creating the Event.")
+        else:
+            print(event_form.cleaned_data)
+            print(event_form.errors)
+        return redirect('my_events')
     else:
         profile_id = Profile.objects.get(user=request.user).id
-        event_form = UpdateEventForm(initial={'owner':profile_id})
+        event_form = UpdateEventForm(initial={'owner': profile_id})
 
     context = {'form': event_form}
     context['title'] = 'Create Event'
     return render(request, 'event_create.html', context)
 
 @login_required
-def update_event_view(request):
+def update_event_view(request, pk):
+    event = get_object_or_404(Event, id=pk)
+    profile_id = Profile.objects.get(user=request.user).id
+    # if logged in user is not owner of the event, then raise 403
+    if profile_id != event.owner_id:
+        raise PermissionDenied("You do not have permission to edit this event")
     if request.method == 'POST':
-        event_form = UpdateEventForm(request.POST)
+        event_form = UpdateEventForm(request.POST, instance=event)
         if event_form.is_valid():
             event_form.save()
-            return redirect('event_list')
+        else:
+            pass
+
+        return redirect('event_list', pk)
     else:
-        event_form = UpdateEventForm()
 
-    context = {'form': event_form}
-    context['title'] = 'Update Event'
-    return render(request, 'event_update.html', context)
+        event_form = UpdateEventForm(instance=event)
+
+        context = {'form': event_form}
+        context['title'] = 'Update Event'
+        return render(request, 'event_update.html', context)
 
 
+@require_http_methods(['GET'])
 def category_list_view(request):
+
     categories = Category.objects.all()
     context = {'category_list': categories}
 
     return render(request, 'category_list.html', context)
 
-
+@login_required
 def create_category_view(request):
     if request.method == 'POST':
         category_form = UpdateCategoryForm(request.POST)
@@ -109,11 +166,12 @@ def create_category_view(request):
     context = {'form': category_form, 'title': 'Create Category'}
     return render(request, 'category_create.html', context)
 
-
+@login_required
 def category_delete_view(request, pk):
     category = get_object_or_404(Category, id=pk)
     if request.method == 'POST':
         category.delete()
+        messages.success(request, "Event deleted")
         return redirect('category_list')
 
     context = {'category': category}
